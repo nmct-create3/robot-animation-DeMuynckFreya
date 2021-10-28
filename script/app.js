c44e1a77a24efa4a97d152823e3e8b5ce31d8fcb
DeMuynckFreya <freya.de.muynck@student.howest.be>
2021-10-28 14:43:07 +0200

    robot animated

--- script/app.js
+++ script/app.js
@@ -9,23 +9,50 @@ let tl = gsap.timeline({
 
 tl.set('#Shadow', {
     transformOrigin: '50% 100%',
+  }).set('#Fire',{
+    transformOrigin: '50% 10%'
   }).fromTo('#Robot',{
     y: 2.5,
 },{
     y: -2.5,
-}).to('#Shadow', {
+}).fromTo('#Head',{
+  y: 0,
+},{
+  y: 1,
+  ease:'sine.out'
+},0).fromTo('#Arm-left',{
+    rotate: 2.5,
+    transformOrigin: 'right top',
+},{
+    rotate: -2.5,
+    transformOrigin: 'right top',
+},0).fromTo('#Arm-right',{
+  rotate: -2.5,
+  transformOrigin: 'left top',
+},{
+  rotate: 2.5,
+  transformOrigin: 'left top',
+},0).to('#Shadow', {
     scale: 0.75,
 },
   '<',
-)
+).to('#Fire', {
+  scale: 1.35,
+  ease: 'expo.Out'
+},
+  '<',)
 
-const btnPlay = document.querySelector(".c-play-icon--play");
-const btnPause = document.querySelector(".c-play-icon--pause");
+
+const btnPause = document.querySelector(".js-pause");
 const btnFast = document.querySelector("#speedFast")
 const btnNormal = document.querySelector("#speedNormal")
 const btnSlow = document.querySelector("#speedSlow")
-btnPause.onclick = () => tl.pause();
-btnPlay.onclick = () => tl.play()
+btnPause.onclick = function () {
+  tl.paused(!tl.paused());
+  btnPause.classList.toggle("is-paused")
+};
+
+
 btnFast.onclick = () => tl.timeScale(2);
 btnNormal.onclick = () => tl.timeScale(1);
 btnSlow.onclick = () => tl.timeScale(.5);
